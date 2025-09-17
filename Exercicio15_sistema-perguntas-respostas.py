5decb88d272edb6e8ef87b748138f3b6053f02fa
JessicaRigol <112140945+JessicaRigol@users.noreply.github.com>
2025-09-17 11:41:04 -0300

    exercicio comentado: uso de dicionário, loops, variaveis de validação.

--- Exercicio15_sistema-perguntas-respostas.py
+++ Exercicio15_sistema-perguntas-respostas.py
@@ -3,7 +3,7 @@ perguntas = [
     {
         'Pergunta': 'Quanto é 2+2?',
         'Opções': ['1', '3', '4', '5'],
-        'Resposta': '4',  # Resposta correta
+        'Resposta': '4',  
     },
     {
         'Pergunta': 'Quanto é 5*5?',
